ec21e73e4435581afb8a249f84c7b66a3990a0b0
wem <wem@kth.se>
2021-05-11 19:44:53 +0200

    Create image instead of setting pixels

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,46 +11,56 @@ void generate_image(sf::Image& image, std::vector<double> interval, int max_iter
 void generate_image(sf::Image& image, std::vector<double> interval, int max_iterations)
 {
 	double h_x = (interval.at(1) - interval.at(0)) / (1.0 * width);
-	double h_y = (interval.at(3) - interval.at(2)) / (1.0 * height);
-	image.create(width, height);
+	double h_y = (interval.at(2) - interval.at(3)) / (1.0 * height);
 	double x0 = interval.at(0);
-	double y0 = interval.at(2);
+	double y0 = interval.at(3);
 	double x, y, x2, y2;
 	bool symmetry;
-
-	std::array<std::array<sf::Uint8, height>, width> n_arr;
+	sf::Uint8* pixels;
+	pixels = new sf::Uint8[4 * width * height]; // Multiplied by 4 because of RGBA
+	int index = 0;
 	auto start = high_resolution_clock::now();
 	for (int i = 0; i < height; i++)
 	{
 		x0 = interval.at(0);
 
-		symmetry = y0 > 0.0 && -y0 >= interval.at(2);
+		symmetry = y0 < 0.0 && -y0 <= interval.at(3);
 
 		for (int j = 0; j < width; j++)
 		{
 			if (symmetry)
 			{
-				int index = (int)((-y0 - interval.at(2)) / h_y);
-				n_arr[j][i] = n_arr[j][index];
-				continue;
+				int reflected_y_index = (int)(-1.0 + (-y0 - interval.at(3)) / h_y);
+				int reflected_index = j + width * reflected_y_index;
+				pixels[index] = pixels[4 * reflected_index];
+				pixels[index + 1] = pixels[4 * reflected_index + 1];
+				pixels[index + 2] = pixels[4 * reflected_index + 2];
+				pixels[index + 3] = pixels[4 * reflected_index + 3];
 			}
-			x = 0.0;
-			y = 0.0;
-			x2 = 0.0;
-			y2 = 0.0;
-			sf::Uint8 n = 0;
-			while (x * x + y * y <= 4 && n < max_iterations)
+			else
 			{
-				y = 2 * x * y + y0;
-				x = x2 - y2 + x0;
-				x2 = x * x;
-				y2 = y * y;
-				n += 1;
+				x = 0.0;
+				y = 0.0;
+				x2 = 0.0;
+				y2 = 0.0;
+				int n = 0;
+				while (x * x + y * y <= 4 && n < max_iterations)
+				{
+					y = 2 * x * y + y0;
+					x = x2 - y2 + x0;
+					x2 = x * x;
+					y2 = y * y;
+					n += 1;
+				}
+
+				pixels[index] = (int)(255.0 * n / 1.0 * max_iterations);
+				pixels[index + 1] = (int)(50.0 * n / 1.0 * max_iterations);
+				pixels[index + 2] = (int)(50.0 * n / 1.0 * max_iterations);
+				pixels[index + 3] = 255;
+
+				x0 += h_x;
 			}
-
-			n_arr[j][i] = n;
-
-			x0 += h_x;
+			index += 4;
 		}
 		y0 += h_y;
 	}
@@ -58,19 +68,11 @@ void generate_image(sf::Image& image, std::vector<double> interval, int max_iter
 	auto duration = duration_cast<microseconds>(stop - start);
 	std::cout << "First loop " << duration.count() << std::endl;
 	start = high_resolution_clock::now();
-	sf::Uint8 n;
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			n = n_arr[j][i];
-			image.setPixel(j, i, sf::Color(255 * n / max_iterations, 50 * n / max_iterations, 50 * n / max_iterations));
-		}
-	}
+	image.create(width, height, &pixels[0]);
 	stop = high_resolution_clock::now();
 	duration = duration_cast<microseconds>(stop - start);
-
 	std::cout << "Second loop " << duration.count() << std::endl;
+	delete[] pixels;
 }
 
 int main()
@@ -94,16 +96,15 @@ int main()
 
 	sf::Texture texture;
 	texture.loadFromImage(image);
-
 	sf::Sprite sprite(texture);
 
 	sf::Event event;
-	window.clear();
 	bool buttonHold = false;
 	int init_x = 0;
 	int init_y = 0;
 	while (window.isOpen())
 	{
+		window.clear();
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
@@ -134,8 +135,8 @@ int main()
 						double delta_y = event.mouseMove.y - init_y;
 						interval.at(0) -= sensitivity * (interval.at(1) - interval.at(0)) * delta_x / (2.0 * width);
 						interval.at(1) -= sensitivity * (interval.at(1) - interval.at(0)) * delta_x / (2.0 * width);
-						interval.at(2) -= sensitivity * (interval.at(3) - interval.at(2)) * delta_y / (2.0 * height);
-						interval.at(3) -= sensitivity * (interval.at(3) - interval.at(2)) * delta_y / (2.0 * height);
+						interval.at(2) += sensitivity * (interval.at(3) - interval.at(2)) * delta_y / (2.0 * height);
+						interval.at(3) += sensitivity * (interval.at(3) - interval.at(2)) * delta_y / (2.0 * height);
 
 						init_x = event.mouseMove.x;
 						init_y = event.mouseMove.y;
@@ -150,8 +151,9 @@ int main()
 			{
 				double zoom_factor = (event.mouseWheelScroll.delta > 0) ? (zoom) : (1.0 / zoom);
 				double mouse_x_before = interval.at(0) + (interval.at(1) - interval.at(0)) * event.mouseWheel.x / (1.0 * width);
-				double mouse_y_before = interval.at(2) + (interval.at(3) - interval.at(2)) * event.mouseWheel.y / (1.0 * height);
-
+				double mouse_y_before = interval.at(3) + (interval.at(2) - interval.at(3)) * event.mouseWheel.y / (1.0 * height);
+				std::cout << mouse_x_before << std::endl;
+				std::cout << mouse_y_before << std::endl;
 				//Zoom
 				interval.at(0) /= zoom_factor;
 				interval.at(1) /= zoom_factor;
@@ -159,7 +161,7 @@ int main()
 				interval.at(3) /= zoom_factor;
 
 				double mouse_x_after = interval.at(0) + (interval.at(1) - interval.at(0)) * event.mouseWheel.x / (1.0 * width);
-				double mouse_y_after = interval.at(2) + (interval.at(3) - interval.at(2)) * event.mouseWheel.y / (1.0 * height);
+				double mouse_y_after = interval.at(3) + (interval.at(2) - interval.at(3)) * event.mouseWheel.y / (1.0 * height);
 
 				//Correction
 				interval.at(0) -= mouse_x_after - mouse_x_before;
